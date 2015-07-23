abccff1f2ce02922e6d89cdd8ded7b20806383db
Panos <panos.kittenis@thomsonreuters.com>
2015-07-23 16:44:50 +0100

    Bumped version

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ with open('README.rst') as f:
 
 setup(
     name='graphite-api',
-    version='1.0.1a',
+    version='1.0.2a',
     url='https://github.com/brutasse/graphite-api',
     author="Bruno Renié, based on Chris Davis's graphite-web",
     author_email='bruno@renie.fr',
